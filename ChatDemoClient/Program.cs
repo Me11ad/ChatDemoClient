af820f7a0d7cf6b78a9dbae592c4c5a5476b74e8
Me11ad <vlad505200483@gmail.com>
2024-02-22 10:13:39 +0500

    Добавьте файлы проекта.

---
+++ ChatDemoClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+try
+{
+    // Подключение к серверу
+    await tcpClient.ConnectAsync("127.0.0.1", 1235);
+    while (true)
+    {
+
+        // Создание сообщения 
+        System.Console.WriteLine("Введите команду для сервера");
+        string command = Console.ReadLine() + '\n';
+
+        // Первод запроса из строкового типа данных в массив байт
+        byte[] requestData = Encoding.UTF8.GetBytes(command);
+        await tcpClient.SendAsync(requestData, new SocketFlags());
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
